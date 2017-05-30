04ec19253748784bb885fb1df2ada45bf60c03fc
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-29 18:04:37 -0700

    Create, sort and print the new dictionary for restaurants rating and made code more dry/readable

--- ratings.py
+++ ratings.py
@@ -1,23 +1,35 @@
 """Restaurant rating lister."""
 
-#import sys
+import sys
 
-def get_dict_ratings(filename):
-    """Opens file, converts each line into a list and separates by : 
-        then  unpacks and converts into dictionary
+def get_ratings(filename):
+    """creating dictionary from a given file, and return it.
     """
-    #filename = sys.argv[1]
-    filename = open(filename)
-    rating_dicts = {}
-    for line in filename:
-        #here it takes a line from the file, rstrip() removes the new line char and all spaces on the right side and split(":") separates string by :
-        sentence = line.rstrip().split(":")
-        # unpacking the list
-        rate_key, rate_value = sentence
+    # filename = sys.argv[1]
+    f = open(filename)
+    ratings = {}
+    for line in f:
+        #here it takes a line from the file, rstrip() removes the new line char
+        # and all spaces on the right side and split(":") separates string by :
+        # and upacks the string.
+        restaurant, rating = line.rstrip().split(":")
         #taking rate_key making it a key in the rating_dicts and rate_value becomes a value in rate_dicts
-        rating_dicts[rate_key] = rate_value
+        ratings[restaurant] = rating
         #Here we tried to unpack sentence inside a for loop it did not work
         #for rate_key, rate_value in sentence:
-    return rating_dicts
-#make separate func for soted dict
-print get_dict_ratings('scores.txt')
+
+    f.close()
+    return ratings
+
+
+
+
+def print_sorted(ratings):
+    """sorts and prints a given dictionary. """
+    for restaurant, rating in sorted(ratings.items()):
+        print "{}: {}".format(restaurant, rating)
+
+
+filename = sys.argv[1]
+
+print_sorted(get_ratings(filename))
